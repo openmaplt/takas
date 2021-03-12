e6953d6680f268ce17abea64f019fc4cea8e3672
tomass <tomasstraupis@gmail.com>
2021-03-12 21:55:40 +0200

    Žemėlapio parametrai pagal url/cookie

--- src/app.js
+++ src/app.js
@@ -2,12 +2,9 @@ import mapboxgl from '!mapbox-gl';
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
 import './styles.css';
 import {version} from '../package.json';
+import { initMap, switchTo, map } from './map.js';
 
 i_version.innerHTML = version;
-var map;
-/* TODO Remove unused declaration 2021-03-07
-var marker;
-var kurtiTaska = false;*/
 var phpBase = 'php/';
 var urlTaskai = phpBase + 'points.php';
 var kuriamMarsruta = false;
@@ -18,20 +15,6 @@ var draw;
 var lastLat = 0;
 var lastLon = 0;
 var orto = false;
-/* TODO Remove unused declaration 2021-03-07
-var data_taskai = {
-  "type": "FeatureCollection",
-  "features": [{
-    "type": "Feature",
-    "geometry": {
-      "type": "Point",
-      "coordinates": [25.63352, 54.88994]
-    },
-    "properties": {
-      "name": "Pavyzdinis taškas"
-    }
-  }]
-};*/
 var markers = [];
 markers[0] = '<svg style="width: 100px" display="block" height="41px" width="27px" viewBox="0 0 27 41"><g fill-rule="nonzero"><g transform="translate(3.0, 29.0)" fill="#000000"><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="10.5" ry="5.25002273"></ellipse><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="10.5" ry="5.25002273"></ellipse><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="9.5" ry="4.77275007"></ellipse><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="8.5" ry="4.29549936"></ellipse><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="7.5" ry="3.81822308"></ellipse><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="6.5" ry="3.34094679"></ellipse><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="5.5" ry="2.86367051"></ellipse><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="4.5" ry="2.38636864"></ellipse></g><g fill="#colour"><path d="M27,13.5 C27,19.074644 20.250001,27.000002 14.75,34.500002 C14.016665,35.500004 12.983335,35.500004 12.25,34.500002 C6.7499993,27.000002 0,19.222562 0,13.5 C0,6.0441559 6.0441559,0 13.5,0 C20.955844,0 27,6.0441559 27,13.5 Z"></path></g><g opacity="0.25" fill="#000000"><path d="M13.5,0 C6.0441559,0 0,6.0441559 0,13.5 C0,19.222562 6.7499993,27 12.25,34.5 C13,35.522727 14.016664,35.500004 14.75,34.5 C20.250001,27 27,19.074644 27,13.5 C27,6.0441559 20.955844,0 13.5,0 Z M13.5,1 C20.415404,1 26,6.584596 26,13.5 C26,15.898657 24.495584,19.181431 22.220703,22.738281 C19.945823,26.295132 16.705119,30.142167 13.943359,33.908203 C13.743445,34.180814 13.612715,34.322738 13.5,34.441406 C13.387285,34.322738 13.256555,34.180814 13.056641,33.908203 C10.284481,30.127985 7.4148684,26.314159 5.015625,22.773438 C2.6163816,19.232715 1,15.953538 1,13.5 C1,6.584596 6.584596,1 13.5,1 Z"></path></g><g transform="translate(6.0, 7.0)" fill="#FFFFFF"></g><g transform="translate(8.0, 8.0)"><circle fill="#000000" opacity="0.25" cx="5.5" cy="5.5" r="5.4999962"></circle><circle fill="#FFFFFF" cx="5.5" cy="5.5" r="5.4999962"></circle></g></g></svg>';
 markers[1] = '<svg style="width: 100px" display="block" height="41px" width="27px" viewBox="0 0 27 41"><g fill-rule="nonzero"><g transform="translate(3.0, 29.0)" fill="#000000"><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="10.5" ry="5.25002273"></ellipse><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="10.5" ry="5.25002273"></ellipse><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="9.5" ry="4.77275007"></ellipse><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="8.5" ry="4.29549936"></ellipse><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="7.5" ry="3.81822308"></ellipse><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="6.5" ry="3.34094679"></ellipse><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="5.5" ry="2.86367051"></ellipse><ellipse opacity="0.04" cx="10.5" cy="5.80029008" rx="4.5" ry="2.38636864"></ellipse></g><g fill="#colour"><path d="M27,13.5 C27,19.074644 20.250001,27.000002 14.75,34.500002 C14.016665,35.500004 12.983335,35.500004 12.25,34.500002 C6.7499993,27.000002 0,19.222562 0,13.5 C0,6.0441559 6.0441559,0 13.5,0 C20.955844,0 27,6.0441559 27,13.5 Z"></path></g><g opacity="0.25" fill="#000000"><path d="M13.5,0 C6.0441559,0 0,6.0441559 0,13.5 C0,19.222562 6.7499993,27 12.25,34.5 C13,35.522727 14.016664,35.500004 14.75,34.5 C20.250001,27 27,19.074644 27,13.5 C27,6.0441559 20.955844,0 13.5,0 Z M13.5,1 C20.415404,1 26,6.584596 26,13.5 C26,15.898657 24.495584,19.181431 22.220703,22.738281 C19.945823,26.295132 16.705119,30.142167 13.943359,33.908203 C13.743445,34.180814 13.612715,34.322738 13.5,34.441406 C13.387285,34.322738 13.256555,34.180814 13.056641,33.908203 C10.284481,30.127985 7.4148684,26.314159 5.015625,22.773438 C2.6163816,19.232715 1,15.953538 1,13.5 C1,6.584596 6.584596,1 13.5,1 Z"></path></g><g transform="translate(6.0, 7.0)" fill="#FFFFFF"></g><g transform="translate(8.0, 8.0)"></circle></g></g></svg>';
@@ -51,49 +34,7 @@ colours.forEach(el => {
 if (!mapboxgl.supported()) {
   alert('Jūsų naršyklė nepalaiko Mapbox GL. Prašome atsinaujinti naršyklę.');
 } else {
-  // TODO: Pakeisti JQuery į vanilla
-  /*if (hashData = getMapDataFromHashUrl()) {
-    $.extend(mapData, hashData);
-  }*/
-
-  /*if (!hashData && (cookieData = readCookie(cookieName))) {
-    $.extend(mapData, cookieData);
-  }
-  $("button[data-style='" + mapData.type + "']").addClass('active');
-  $('#layers').removeClass('hidden');*/
-
-  /*changeHashUrl();*/
-
-  map = new mapboxgl.Map({
-    container: 'map',
-    /*style: mapData.type + '.json',*/
-    style: 'topo.json',
-    zoom: 12,
-    minZoom: 7,
-    maxZoom: 18,
-    center: /*pCenter ||*/ [25.33762, 54.76875],
-    hash: false,
-    maxBounds: [20.700, 53.700, 27.050, 56.650],
-    /*bearing: mapData.bearing,
-    pitch: mapData.pitch,*/
-    attributionControl: false
-  })
-    .addControl(new mapboxgl.NavigationControl(), 'top-left')
-    .addControl(new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true
-      },
-      trackUserLocation: true
-    }), 'top-left')
-    .addControl(new mapboxgl.AttributionControl(), 'bottom-left')
-    .on('moveend', function () {
-      /*setMapData();*/
-      /*changeHashUrl();*/
-    })
-    .on('load', function() {
-      runApp();
-    })
-  ;
+  initMap(runApp);
 }
 
 i_prideti_taska.onclick = pridetiTaska;
@@ -116,7 +57,8 @@ function getIcon(pIconId, pColour) {
   return markers[pIconId].replaceAll('#colour', pColour);
 } // getIcon
   function setUrl(uuid) {
-    i_url.innerHTML = '<small>TODO.marsrutas.html?uuid=' + uuid + '</small>';
+    i_url.innerHTML = '<small>' + window.location.protocol + '//' +
+      window.location.host.toString() + '/route.html?uuid=' + uuid + '</small>';
   } // setUrl
   function addPointLayer() {
     var dummySource = map.getSource('taskai-src');
@@ -305,13 +247,6 @@ function getIcon(pIconId, pColour) {
   }
   function drawSelectionchange(e) {
     setMarkersMovable(e.features.length == 0);
-  }
-  function switchTo(m) {
-    map.setStyle(m + '.json');
-    /* TODO reimplement this:
-    mapData.type = m;
-    mapData.id = null;
-    changeHashUrl(mapData);*/
   }
   function switchBase() {
     if (orto) {
---
+++ src/map.js
@@ -0,0 +1,155 @@
+import mapboxgl from '!mapbox-gl';
+
+var defaultType = 'topo';
+var defaultLat = 54.76875;
+var defaultLng = 25.33762;
+var defaultZoom = 12;
+var cookieName = 'Data';
+var hashData;
+var map;
+
+var mapTypes = mapTypes || {
+  topo: 't',
+  hybrid: 'h'
+};
+var mapData = {
+  type: defaultType,
+  zoom: defaultZoom,
+  lat: defaultLat,
+  lng: defaultLng,
+  bearing: 0,
+  pitch: 0
+};
+  
+function getMapDataFromHashUrl() {
+  var hash = window.location.hash;
+  if (hash.length > 0) {
+    hash = hash.replace('#', '');
+
+    var mapQueries = hash.split('/');
+  
+    // Add support old hash format: #8/55.23777/23.871
+    if (mapQueries.length === 3) {
+      return {
+        zoom: parseFloat(mapQueries[0]),
+        lat: parseFloat(mapQueries[1]),
+        lng: parseFloat(mapQueries[2])
+      }
+    }
+  
+    if (mapQueries.length < 6) {
+      return null;
+    }
+  
+    var type = defaultType;
+    for (var key in mapTypes) {
+      if (mapTypes[key] === mapQueries[0]) {
+        type = key;
+        break;
+      }
+    }
+  
+    return {
+      type: type,
+      zoom: parseFloat(mapQueries[1]),
+      lat: parseFloat(mapQueries[2]),
+      lng: parseFloat(mapQueries[3]),
+      bearing: parseInt(mapQueries[4]),
+      pitch: parseInt(mapQueries[5]),
+      objectId: mapQueries[6] || null
+    };
+  }
+  return null;
+} // getMapDataFromHashUrl
+
+function storeCookie(name, value) {
+  var date = new Date();
+  date.setDate(date.getDate() + 365);
+  var expires = "expires=" + date.toUTCString();
+  document.cookie = name + '=' + JSON.stringify(value) + ';domain=.' + window.location.host.toString() + ';path=/;' + expires;
+} // storeCookie
+  
+function readCookie(name) {
+  var result = document.cookie.match(new RegExp(name + '=([^;]+)'));
+  if (result) {
+    return JSON.parse(result[1]);
+  }
+    return null;
+} // readCookie
+
+function switchTo(m) {
+  map.setStyle(m + '.json');
+    mapData.type = m;
+    mapData.id = null;
+    changeHashUrl(mapData);
+}
+
+function setMapData() {
+  mapData.zoom = Number(map.getZoom().toFixed(2));
+  mapData.lat = Number(map.getCenter().lat.toFixed(5));
+  mapData.lng = Number(map.getCenter().lng.toFixed(5));
+  mapData.bearing = parseInt(map.getBearing());
+  mapData.pitch = parseInt(map.getPitch());
+} // setMapData
+
+function getUrlHash(state) {
+  var hash = [];
+  for (var key in state) {
+    var value = state[key];
+    if (key === 'type') {
+      value = mapTypes[state.type]
+    }
+    hash.push(value);
+  }
+  return hash.join('/');
+} // getUrlHash
+  
+function changeHashUrl() {
+  var hash = getUrlHash(mapData);
+  window.location.hash = '#' + hash;
+  storeCookie(cookieName, mapData);
+} // changeHashUrl
+  
+function initMap(callback) {
+  var cookieData;
+  if (hashData = getMapDataFromHashUrl()) {
+    mapData = Object.assign(mapData, hashData);
+  }
+
+  if (!hashData && (cookieData = readCookie(cookieName))) {
+    mapData = Object.assign(mapData, hashData);
+  }
+
+  changeHashUrl();
+
+  map = new mapboxgl.Map({
+    container: 'map',
+    style: mapData.type + '.json',
+    zoom: mapData.zoom,
+    minZoom: 7,
+    maxZoom: 18,
+    center: [mapData.lng, mapData.lat],
+    hash: false,
+    maxBounds: [20.700, 53.700, 27.050, 56.650],
+    bearing: mapData.bearing,
+    pitch: mapData.pitch,
+    attributionControl: false
+  })
+    .addControl(new mapboxgl.NavigationControl(), 'top-left')
+    .addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true
+    }), 'top-left')
+    .addControl(new mapboxgl.AttributionControl(), 'bottom-left')
+    .on('moveend', function () {
+      setMapData();
+      changeHashUrl();
+    })
+    .on('load', function() {
+      callback();
+    })
+  ;
+} // initMap
+export { initMap, switchTo, map };
